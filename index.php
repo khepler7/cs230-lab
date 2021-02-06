85bf1e3983538cb5f089035863e4f6a1f33247e6
khepler7 <kmh0071@mix.wvu.edu>
2021-02-05 23:49:22 -0500

    Lab 2

---
+++ index.php
@@ -0,0 +1,50 @@
+<main>
+    <link rel="stylesheet" href="css/about.css">
+   
+    <style>
+        body {
+            background-image: url('images/background.jpg');
+            background-size: 100% auto;
+        }
+    </style>
+
+    <div class="about">
+        <h1 style="font-size:50px">ABOUT US</h1>
+        <p>------------------------------------------------------------------------------------------------------</p>
+    </div>
+
+
+    <div class="row">
+        <div class="column">
+            <div class="card">
+            <img src="images/wvu.jpeg">
+                <div class="container">
+                    <h2>The Reviewer</h2>
+                    <p>My name is Kate and I am a computer science student at West Virginia University. My current TV show obsession is Marvel's WandaVision. This webite contains my reviews and theories for each new episode.</p>
+                    <p>Email: kmh0071@mix.wvu.edu</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/wandavisionposter.jpg">
+                <div class="container">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/wanda.jpg">
+                <div class="container">
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</main>
